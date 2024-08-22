ee43ca0600715dfd51ccebc9faf7caa0e156fff5
Mehefuj Ali <mehefujali30@gmail.com>
2024-08-22 09:02:29 +0530

    all file created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'rintero-primary-color': '#ABEF5F'
+            }
+        },
+    },
+    plugins: [],
+}
